bc9ba3b7b92853fb8ce6898492b1575c7a60e09b
JosueLanda <josuelanda11@gmail.com>
2023-01-23 19:12:44 -0600

    develop

---
+++ challenge/easy/contando_en_la_arena.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+
+    long numero;
+
+    while (1)
+    {
+        scanf("%ld\n", &numero);
+        if (numero == 0)
+        {
+            break;
+        }
+        for (; numero > 0; numero--)
+        {
+            printf("1");
+        }
+        printf("\n");
+    }
+    return 0;
+}
