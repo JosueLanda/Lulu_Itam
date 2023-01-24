bb84de5d8d1622d35ed3733a2b84b0223d97059d
LuisMAC2022 <317201273@pcpuma.acatlan.unam.mx>
2023-01-24 00:31:24 -0600

    bienvenido

---
+++ challenge/easy/bienvenido.c
@@ -0,0 +1,22 @@
+/*Title: Problema "Bienvenido a la FES"
+Author: Luis Daniel Martínez Arana
+Date: Jan 23 2023
+Last Modified: __
+Description: Escribe "FES ACATLAN TU SEGUNDA CASA" cuantas veces sea necesario
+*/
+#include <stdio.h>
+
+int main()
+{
+int entrada;
+scanf("%d",&entrada);
+while (entrada--) printf("FES ACATLAN TU SEGUNDA CASA\n");
+
+	return 1;
+}
+
+
+
+
+
+
